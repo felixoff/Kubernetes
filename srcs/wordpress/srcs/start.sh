62cfea7d09943c6320860e6ffaf86a0b8aa86972
Suguha Jennette <sjennett@oa-l5.msk.21-school.ru>
2021-07-04 20:45:01 +0300

    updated

---
+++ srcs/wordpress/srcs/start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Start Telegraf and Wordpress.
+# php --help (-S, --server; Start the internal web server / -t, --docroot; Specify the root of the document for the internal web server).
+/usr/bin/supervisord -c /etc/supervisord.conf
